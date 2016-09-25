8d263b56dda20f59ed9a4d63d9fc030d0499a1ef
Steven Avrenli <steven.avrenli@mckenziewagner.com>
2016-09-24 21:46:21 -0400

    Initial Commit

---
+++ Randopaper/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Randopaper
+//
+//  Created by Steven Avrenli on 9/24/16.
+//  Copyright © 2016 Avrenli Design. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
